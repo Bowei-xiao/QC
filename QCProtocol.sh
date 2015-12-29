7cbf15d7ecc25f729a9f71fdc80fc7a3f072c27c
Bowei-xiao <work.adam.xiao@gmail.com>
2015-12-29 11:49:34 -0500

    Update QCProtocol.sh
    Added custom threshold for missing rates.

--- QCProtocol.sh
+++ QCProtocol.sh
@@ -1,8 +1,12 @@
 # !/bin/bash
 
 #QC script
+# Syntax: bash QCProtocol.sh ${PlinkFileName} 0.03 0.05
 # would assume this is the binary format
 plinkOri=$1
+indRate=$2 #Individual missing rate, I normally use 0.03
+snpRate=$3 #SNP missing rate, I normally use 0.05
+
 # We want noPedID.txt to indicate whom we want to throw away
 prefix=1
 scriptADS=/hpf/projects/arnold/bowei/ADHD_ClinicalData
@@ -14,13 +18,13 @@ plink --noweb --bfile ${prefix}_${plinkOri}_withPID --missing --out ${prefix}_${
 #Fix the blank; change delimiter to single blank
 awk '{gsub(" ?","",$1)}1' ${prefix}_${plinkOri}.imiss > ${prefix}_${plinkOri}_blankFix.imiss
 awk '{gsub(" ?","",$1)}1' ${prefix}_${plinkOri}.lmiss > ${prefix}_${plinkOri}_blankFix.lmiss
-# Filter Inds with missingness >= 3%
-awk '$6 <= 0.03' ${prefix}_${plinkOri}_blankFix.imiss | cut -f1,2 > ${prefix}_${plinkOri}IndLowMiss.txt
+# Filter Inds with missingness >= the preset rate
+awk -v miss="$indRate" '$6 <= miss' ${prefix}_${plinkOri}_blankFix.imiss | cut -f1,2 > ${prefix}_${plinkOri}IndLowMiss.txt
 prefix_old=$prefix
 prefix=$(( $prefix + 1 ))
 plink --noweb --bfile ${plinkOri} --keep ${prefix_old}_${plinkOri}IndLowMiss.txt --make-bed --out ${prefix}_${plinkOri}_highMissIndRMV
-# remove SNPs with call rate less than 95%
-awk '$5 <= 0.05' ${prefix_old}_${plinkOri}_blankFix.lmiss | cut -f2 > ${prefix}_${plinkOri}_highCallSNP.txt
+# remove SNPs with call rate that are more than pre-set rate
+awk -v miss="$snpRate" '$5 <= miss' ${prefix_old}_${plinkOri}_blankFix.lmiss | cut -f2 > ${prefix}_${plinkOri}_highCallSNP.txt
 prefix_old=$prefix
 prefix=$(( $prefix + 1 ))
 plink --noweb --bfile ${prefix_old}_${plinkOri}_highMissIndRMV --extract ${prefix_old}_${plinkOri}_highCallSNP.txt --make-bed --out ${prefix}_${plinkOri}_highMissIndSNPRMV
@@ -71,7 +75,8 @@ cp ${prefix}_${plinkOri}_HWE.bim ${plinkOri}_afterQC.bim
 cp ${prefix}_${plinkOri}_HWE.bed ${plinkOri}_afterQC.bed
 cp ${prefix}_${plinkOri}_HWE.fam ${plinkOri}_afterQC.fam 
 # recording individuals that were removed in the QC
-awk '$6 > 0.03 && $1 + 0 == $1' 1_${plinkOri}_blankFix.imiss | cut -d' ' -f1,2 > TMP_lowRate.txt
+# $1+=0 == $1 used to remove header, it's not perfect, can only deal with header strating with Characers not numbers
+awk -v miss="$indRate" $6 > miss && $1 + 0 == $1' 1_${plinkOri}_blankFix.imiss | cut -d' ' -f1,2 > TMP_lowRate.txt
 yes 'Low call Rate' | head -n `wc -l TMP_lowRate.txt | cut -d' ' -f1` > TMP_lowRateReason.txt
 yes 'Failed heteo check' | head -n `wc -l 4_${plinkOri}_indNeedremove.txt | cut -d' ' -f1` > TMP_heteoReason.txt
 yes 'No pedigree ID found' | head -n `wc -l noPedID.txt | cut -d' ' -f1` > TMP_noPedReason.txt
